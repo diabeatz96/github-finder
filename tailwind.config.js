2e6cabe35f88629bda34a561d04d84c32a02a4aa
bigbl <adamkostandy@gmail.com>
2022-03-18 08:01:41 -0400

    INIT: Initial Setup, TailwindCSS, DaisyUI, Reactrouterdom, ReactIcons

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    darkMode: 'class',
+  content: [
+      './public/**/*.html',
+      './src/**/*.{js,jsx,ts,tsx,vue}',],
+  theme: {
+      extend: {},
+  },
+  plugins: [require('daisyui')],
+}
