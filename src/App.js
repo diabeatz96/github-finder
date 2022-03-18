7b14d3b51e0275a3cc4ca7da90f0afd9257bae28
bigbl <adamkostandy@gmail.com>
2022-03-18 08:37:51 -0400

    FEAT: Nav-bar added. Tailwind CSS provided fpr all.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-hl">Hello World</h1>
+      <Router>
 
-        <button className='btn'>Click</button>
-    </div>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar/>
+
+          <main>Content</main>
+
+        </div>
+
+      </Router>
   );
 }
 
